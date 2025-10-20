6c9c8cac548ab9c45af3385caba46af73fddc1fe
tameemahamed <ahamedtameem00@gmail.com>
2025-10-21 00:46:18 +0600

    updated users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('username')->unique();
-            $table->string('profile_picture_url');
+            $table->string('profile_picture_url')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
