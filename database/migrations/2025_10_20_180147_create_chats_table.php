7cc3864c6d0478b70f46109bcc789cbe5acf0180
tameemahamed <ahamedtameem00@gmail.com>
2025-10-21 00:23:10 +0600

    added database migration

---
+++ database/migrations/2025_10_20_180147_create_chats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('sender_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignUuid('receiver_id')->constrained('users')->cascadeOnDelete();
+            $table->longText('message');
+            $table->timestamps();
+            $table->timestamp('delivered_at')->nullable();
+            $table->timestamp('read_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
